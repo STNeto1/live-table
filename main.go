7b3c812d80059533bb8ba4dae0a56116a0bf65e0
STNeto1 <gfreitasneto18@gmail.com>
2023-12-14 12:16:36 -0300

    fix air and add root templ

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "log"
+import (
+	"github.com/a-h/templ"
+	"github.com/gofiber/fiber/v2"
+	"github.com/stneto1/htmx-webcomponents/views"
+)
 
 func main() {
-	log.Println("Hello")
+	app := fiber.New(fiber.Config{})
+	// app.Use(logger.New())
+	app.Static("/public", "./public")
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		btn := views.RootLayout("Page Title")
+
+		return render(btn, c)
+	})
+
+	app.Listen(":3000")
+}
+
+func render(component templ.Component, c *fiber.Ctx) error {
+	c.Response().Header.SetContentType("text/html")
+	return component.Render(c.Context(), c.Response().BodyWriter())
 }
